ce9511a34c2bbdcd38f9955804b0a7167b28c76d
Nino Nikoles <nnikoles@gmail.com>
2026-02-02 22:37:05 +0100

    Update Gruntfile.cjs
    sass

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-    const sass = require('node-sass');
+    const sass = require('sass');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
